29ebd554d27ce132c7744dc9522bd24ef09b9e84
zehan12 <zehan9211@gmail.com>
2024-06-08 13:00:00 +0530

    remove code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+
+      <body className={inter.className}>
+        <main>
+          <header>
+            <nav>
+              <ul className="flex gap-4">
+                  <Link href="/" >
+                    home
+                  </Link>
+                  <Link href="/notes" >
+                    notes
+                  </Link>
+              </ul>
+            </nav>
+          </header>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
